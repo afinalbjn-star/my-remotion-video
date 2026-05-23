f55e61285e2464dbea2f25412be6a8a66499e177
Afinal firnando <afinalbjn@gmail.com>
2026-05-23 16:35:57 +0700

    Add fantastic TechLoop workflow and composition

--- Root.tsx
+++ Root.tsx
@@ -3,6 +3,7 @@ import { Composition } from 'remotion';
 import { BloodStream } from './BloodCells';
 import { CyberFlow } from './src/CyberFlow';
 import { SimpleVideo } from './src/SimpleVideo';
+import { TechLoopBackground } from './src/TechLoopBackground';
 
 export const RemotionRoot: React.FC = () => {
     return (
@@ -30,6 +31,14 @@ export const RemotionRoot: React.FC = () => {
                 fps={30}
                 width={1920}
                 height={1080}
+            />
+            <Composition
+                id="TechLoopBackground-4K"
+                component={TechLoopBackground}
+                durationInFrames={600}
+                fps={60}
+                width={3840}
+                height={2160}
             />
         </>
     );
