0e0cebe73e0ded0139e1156259719150cfdb6ecf
Afinal firnando <afinalbjn@gmail.com>
2026-06-22 10:56:53 +0700

    feat: add RiverFlow composition and update render workflow

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,10 +1,16 @@
+// path: src\Root.tsx
+
 import React from 'react';
 import { Composition } from 'remotion';
 import { DataMesh } from './DataMesh';
-import { WinterSkyScene, VIDEO_CONFIG } from './WinterScene';
-import SineWaveSilk from './SineWaveSilk'; // Menggunakan default import agar lebih stabil
+import { WinterSkyScene, VIDEO_CONFIG as WinterSkyConfig } from './WinterScene';
+import RiverFlow, { VIDEO_CONFIG as RiverFlowConfig } from './RiverFlow';
+import SineWaveSilk from './SineWaveSilk';
+import { TechBackground } from './TechBackground';
+import { TechBackgroundComplex } from './TechBackgroundComplex';
+import MyVideo from './MyVideo'; // Impor komponen MyVideo
+import { VIDEO_CONFIG } from './index'; // VIDEO_CONFIG dari index untuk komposisi lain
 
-// Komponen Root yang mendaftarkan semua komposisi
 export const RemotionRoot: React.FC = () => {
     return (
         <>
@@ -19,15 +25,47 @@ export const RemotionRoot: React.FC = () => {
             <Composition
                 id="WinterSky"
                 component={WinterSkyScene}
+                durationInFrames={WinterSkyConfig.durationInFrames}
+                fps={WinterSkyConfig.fps}
+                width={WinterSkyConfig.width}
+                height={WinterSkyConfig.height}
+            />
+            <Composition
+                id="RiverFlow"
+                component={RiverFlow}
+                durationInFrames={RiverFlowConfig.durationInFrames}
+                fps={RiverFlowConfig.fps}
+                width={RiverFlowConfig.width}
+                height={RiverFlowConfig.height}
+            />
+            <Composition
+                id="SineWaveSilk"
+                component={SineWaveSilk}
                 durationInFrames={VIDEO_CONFIG.durationInFrames}
                 fps={VIDEO_CONFIG.fps}
                 width={VIDEO_CONFIG.width}
                 height={VIDEO_CONFIG.height}
             />
             <Composition
-                id="SineWaveSilk"
-                component={SineWaveSilk} // Ini akan mengambil SineWaveSilk dari file .tsx
+                id="TechBackground"
+                component={TechBackground}
                 durationInFrames={VIDEO_CONFIG.durationInFrames}
+                fps={VIDEO_CONFIG.fps}
+                width={VIDEO_CONFIG.width}
+                height={VIDEO_CONFIG.height}
+            />
+            <Composition
+                id="TechBackgroundComplex"
+                component={TechBackgroundComplex}
+                durationInFrames={VIDEO_CONFIG.durationInFrames}
+                fps={VIDEO_CONFIG.fps}
+                width={VIDEO_CONFIG.width}
+                height={VIDEO_CONFIG.height}
+            />
+            <Composition
+                id="MyVideo"
+                component={MyVideo}
+                durationInFrames={VIDEO_CONFIG.durationInFrames} // 10 detik pada 60fps
                 fps={VIDEO_CONFIG.fps}
                 width={VIDEO_CONFIG.width}
                 height={VIDEO_CONFIG.height}
